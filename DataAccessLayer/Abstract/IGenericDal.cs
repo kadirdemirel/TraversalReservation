04cd759f61b5531009cd417c78b904d567d17e28
Kadir Demirel <Kadirdemirel_17@hotmail.com>
2021-12-22 12:31:27 +0300

    IGenericDal oluşturuldu.

---
+++ DataAccessLayer/Abstract/IGenericDal.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Abstract
+{
+    public interface IGenericDal<T>
+    {
+        void Add(T t);
+        void Delete(T t);
+        void Update(T t);
+        List<T> GetAll();
+    }
+}
