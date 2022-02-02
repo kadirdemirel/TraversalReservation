cef9ecc5d86c2eacc84d4d11be78fb7cb2454e55
Kadir Demirel <Kadirdemirel_17@hotmail.com>
2022-02-02 13:02:16 +0300

    Business katmanı için Abstract, Concrete ve FluentValidation klasörleri oluşturuldu. IGenericService oluşturuldu. İlgili entitiy'lerin service'leri oluşturuldu. AboutManager oluşturuldu.

---
+++ BusinessLayer/Abstract/IGenericService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Abstract
+{
+    public interface IGenericService<T>
+    {
+        void Add(T t);
+        void Delete(T t);
+        void Update(T t);
+        List<T> GetAll();
+        T GetById(int id);
+    }
+}
---
+++ BusinessLayer/Concrete/AboutManager.cs
@@ -0,0 +1,44 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class AboutManager : IAboutService
+    {
+        IAboutDal _aboutDal;
+        public AboutManager(IAboutDal aboutDal)
+        {
+            _aboutDal = aboutDal;
+        }
+        public void Add(About about)
+        {
+            _aboutDal.Add(about);
+        }
+
+        public void Delete(About about)
+        {
+            _aboutDal.Delete(about);
+        }
+
+        public List<About> GetAll()
+        {
+            return _aboutDal.GetAll();
+        }
+
+        public About GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(About about)
+        {
+            _aboutDal.Update(about);
+        }
+    }
+}
