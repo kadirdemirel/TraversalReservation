3f3f24917db32b94e5cccdab4470eab8b3d2c06f
Kadir Demirel <Kadirdemirel_17@hotmail.com>
2022-02-02 13:30:49 +0300

    UI katmanında FluentValidation ve FluentValidation.AspNetCore paketleri kuruldu.

---
+++ BusinessLayer/ValidationRules/FluentValidation/AboutValidator.cs
@@ -0,0 +1,21 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules.FluentValidation
+{
+    public class AboutValidator : AbstractValidator<About>
+    {
+        public AboutValidator()
+        {
+            RuleFor(x => x.Description).NotEmpty().WithMessage("Açıklama kısmını boş geçemezsiniz...");
+            RuleFor(x => x.Image).NotEmpty().WithMessage("Lütfen görsel seçiniz...");
+            RuleFor(x => x.Description).MinimumLength(3).WithMessage("Lütfen en az 3 karakterlik açıklama bilgisi giriniz...");
+            RuleFor(x => x.Description).MaximumLength(500).WithMessage("Lütfen en fazla 500 karakterlik açıklama bilgisi giriniz...");
+        }
+    }
+}
